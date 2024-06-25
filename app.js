8eba072fa3f26e4dcf6ffbf75a1f3344d6383d84
rahimbek027 <xabibullayevrahimbek5@gmail.com>
2024-06-26 04:50:21 +0500

    message

---
+++ app.js
@@ -0,0 +1,81 @@
+// masala 1
+
+
+
+let numbers = [-2,10,8,-3]
+
+{
+    let result = []
+
+    numbers.forEach((son) => {
+        if (son>=0){
+            result.push(son)
+        }
+    })
+    console.log(result);
+}
+
+
+
+// masala 2
+
+/*
+const hi = (str) => {
+    return str.split("").reverse().join("");
+  };
+  console.log(hi("salom"));
+
+  */
+
+  // masala 3
+
+  /*
+  const num = (str) => {
+    return str.split("").reverse().join("");
+  };
+  console.log(num(43532627 + ""));
+
+  */
+
+
+  //masala 4
+
+  /*
+  let boolean = [true,false,true,false,true,false]
+
+{
+    let result = []
+
+    boolean.forEach((bool) => {
+        if (bool === false){
+            result.push(bool)
+        }
+    })
+    console.log(result);
+}
+
+*/
+
+// masala 6
+/*
+let numbers = [5,2,3]
+let newArray = []
+
+numbers.forEach(n => {
+    newArray.push(n**2)
+})
+console.log(newArray);
+*/
+
+//masala 7
+/*
+function num(n) {
+    let sum = [];
+    for (let i = 1; n >= i; i++) {
+      sum.push(i);
+    }
+    return sum;
+  }
+  console.log(num(10));
+
+*/
